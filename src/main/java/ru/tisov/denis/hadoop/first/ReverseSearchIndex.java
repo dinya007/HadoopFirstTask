1cf4a79a5890178c2644e87a05ba665b7262dcf1
denis <tlasikee93>
2016-06-05 14:44:18 +0300

    stable version

--- src/main/java/ru/tisov/denis/hadoop/first/ReverseSearchIndex.java
+++ src/main/java/ru/tisov/denis/hadoop/first/ReverseSearchIndex.java
@@ -1,25 +1,28 @@
 package ru.tisov.denis.hadoop.first;
 
-import com.google.common.collect.ComparisonChain;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.StringUtils;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.InputSampler;
+import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
 
-import java.io.DataInput;
-import java.io.DataOutput;
 import java.io.IOException;
-import java.util.Objects;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
 
 import static org.apache.commons.lang.StringUtils.isEmpty;
 
@@ -27,164 +30,175 @@ public class ReverseSearchIndex {
 
     private static final String SPACE = "\\s+";
     private static final String NON_CHARACTER = "[^A-Za-z0-9 ]";
+    private static final int NUM_REDUCE_TASKS = 5;
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
 
         FileSystem fileSystem = FileSystem.newInstance(conf);
 
-        fileSystem.deleteOnExit(new Path(args[1]));
-        fileSystem.delete(new Path(args[2]), true);
+        Path firstJobInput = new Path(args[0]);
+        Path firstJobOutput = new Path(args[1] + "-inter");
+        Path secondJobOutput = new Path(args[2]);
 
-        Job firstJob = Job.getInstance(conf, "First Job");
-        firstJob.setJarByClass(ReverseSearchIndex.class);
-        firstJob.setMapperClass(FirstMapper.class);
-        firstJob.setReducerClass(FirstReducer.class);
-        firstJob.setOutputKeyClass(Text.class);
-        firstJob.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(firstJob, new Path(args[0]));
-        FileOutputFormat.setOutputPath(firstJob, new Path(args[1]));
+        fileSystem.deleteOnExit(firstJobOutput);
+        fileSystem.delete(secondJobOutput, true);
 
-        boolean firstJobSuccessfullyFinished = firstJob.waitForCompletion(true);
+        Job firstMapJob = initFirstJob(conf, firstJobInput, firstJobOutput);
 
-        if (!firstJobSuccessfullyFinished) System.exit(1);
+        if (!firstMapJob.waitForCompletion(true)) System.exit(1);
 
-        Job secondJob = Job.getInstance(conf, "Second Job");
-        secondJob.setJarByClass(ReverseSearchIndex.class);
-        secondJob.setMapperClass(SecondMapper.class);
-        secondJob.setReducerClass(SecondReducer.class);
-
-//        secondJob.setInputFormatClass(KeyValueTextInputFormat.class);
-
-        secondJob.setOutputKeyClass(CompoundKey.class);
-        secondJob.setOutputValueClass(Text.class);
-
-        FileInputFormat.addInputPath(secondJob, new Path(args[1]));
-        FileOutputFormat.setOutputPath(secondJob, new Path(args[2]));
+        Job secondJob = initSecondJob(conf, firstJobOutput, secondJobOutput);
 
         secondJob.waitForCompletion(true);
-
     }
 
-    public static class FirstMapper extends Mapper<Object, Text, Text, Text> {
+    private static Job initSecondJob(Configuration conf, Path inputPath, Path mapOutputPath) throws IOException {
+        Job job = Job.getInstance(conf, "DictionarySorter");
+        job.setJarByClass(ReverseSearchIndex.class);
+        job.setMapperClass(SecondMapper.class);
+        job.setReducerClass(SecondReducer.class);
+        job.setPartitionerClass(TotalOrderPartitioner.class);
+        job.setNumReduceTasks(NUM_REDUCE_TASKS);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputValueClass(Text.class);
+        job.setSortComparatorClass(SortComparator.class);
+
+        FileInputFormat.setInputPaths(job, inputPath);
+        FileOutputFormat.setOutputPath(job, new Path(mapOutputPath
+                + ".dictionary.sorted." + getCurrentDateTime()));
+        job.setPartitionerClass(TotalOrderPartitioner.class);
+
+        Path inputDir = new Path(inputPath + "partitioner");
+        Path partitionFile = new Path(inputDir, "partitioning");
+        TotalOrderPartitioner.setPartitionFile(job.getConfiguration(),
+                partitionFile);
+
+        InputSampler.Sampler sampler = getSampler();
+        try {
+            InputSampler.writePartitionFile(job, sampler);
+        } catch (ClassNotFoundException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return job;
+    }
+
+    private static InputSampler.Sampler getSampler() {
+        double pcnt = 10.0;
+        int numSamples = NUM_REDUCE_TASKS;
+        int maxSplits = NUM_REDUCE_TASKS - 1;
+        if (0 >= maxSplits)
+            maxSplits = Integer.MAX_VALUE;
+
+        return new InputSampler.RandomSampler(pcnt,
+                numSamples, maxSplits);
+    }
+
+    private static Job initFirstJob(Configuration conf, Path mapInputPath, Path mapOutputPath) throws IOException {
+        Job firstMapJob = Job.getInstance(conf, "First Map");
+        firstMapJob.setJarByClass(ReverseSearchIndex.class);
+        firstMapJob.setMapperClass(FirstMapper.class);
+        firstMapJob.setOutputKeyClass(Text.class);
+        firstMapJob.setOutputValueClass(Text.class);
+        TextInputFormat.setInputPaths(firstMapJob, mapInputPath);
+
+        firstMapJob.setNumReduceTasks(NUM_REDUCE_TASKS);
+        firstMapJob.setReducerClass(FirstReducer.class);
+        FileInputFormat.addInputPath(firstMapJob, mapInputPath);
+        FileOutputFormat.setOutputPath(firstMapJob, mapOutputPath);
+        return firstMapJob;
+    }
+
+    private static String getCurrentDateTime() {
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+        return sdf.format(d);
+    }
+
+    private static class FirstMapper extends Mapper<Object, Text, Text, Text> {
 
         private Text word = new Text();
         private Text id = new Text();
 
+        @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] lines = value.toString().split("\n");
+            String[] articles = value.toString().split("\n");
 
-            for (int i = 0; i < lines.length; i++) {
-                String[] words = lines[i].split(SPACE);
+            Set<String> uniqueWords;
+
+            for (String article : articles) {
+                String[] words = article.split(SPACE);
                 id.set(words[0]);
 
-                // Words for checking: Czechs Carmelite
+                uniqueWords = new HashSet<>();
+
                 for (int j = 1; j < words.length; j++) {
                     String candidateWord = words[j].replaceAll(NON_CHARACTER, "").trim();
-                    if (!isEmpty(candidateWord)) {
-                        word.set(candidateWord);
-                        context.write(word, id);
-                    }
+                    if (!isEmpty(candidateWord)) uniqueWords.add(candidateWord);
+                }
+
+                for (String uniqueWord : uniqueWords) {
+                    word.set(uniqueWord);
+                    context.write(word, id);
                 }
 
             }
         }
     }
 
-    public static class SecondMapper extends Mapper<Object, Text, CompoundKey, Text> {
-
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] occurrences = value.toString().split(SPACE);
-
-            String[] result = new String[occurrences.length - 1];
-            System.arraycopy(occurrences, 1, result, 0, result.length);
-
-            String values = StringUtils.join(" ", result);
-
-            context.write(new CompoundKey(occurrences.length - 1, new Text(occurrences[0])), new Text(values));
-        }
-
-    }
-
-    public static class FirstReducer extends Reducer<Text, Text, Text, Text> {
+    private static class FirstReducer extends Reducer<Text, Text, LongWritable, Text> {
+        @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            StringBuilder stringBuilder = new StringBuilder();
-            Set<String> sortedSetFromIterable = getSortedSetFromIterable(values);
-
-            for (String value : sortedSetFromIterable) {
-                stringBuilder.append(" ").append(value);
+            if (key.toString().startsWith("T")) {
+                System.out.println(key);
             }
+            Set<Text> result = new HashSet<>();
+            for (Text value : values) {
 
-            context.write(key, new Text(stringBuilder.toString().trim()));
+                result.add(value);
+            }
+            context.write(new LongWritable((long) result.size()), key);
         }
     }
 
-    public static class SecondReducer extends Reducer<CompoundKey, Text, Text, Text> {
-        public void reduce(CompoundKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            context.write(key.key, new Text(String.valueOf(values.iterator().next().toString().split(SPACE).length)));
+    private static class SecondMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            context.write(key, value);
+        }
+
+    }
+
+    private static class SecondReducer extends Reducer<LongWritable, Text, Text, LongWritable> {
+        @Override
+        public void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            Iterator<Text> iterator = values.iterator();
+            if (iterator.hasNext()) context.write(iterator.next(), key);
         }
     }
 
-    public static class CompoundKey implements WritableComparable<CompoundKey> {
+    private static class SortComparator extends WritableComparator {
 
-        private Integer count = 0;
-        private Text key;
-
-        public CompoundKey() {
-
-        }
-
-        public CompoundKey(Integer count, Text key) {
-            this.count = count;
-            this.key = key;
+        protected SortComparator() {
+            super(LongWritable.class, true);
         }
 
         @Override
-        public int compareTo(CompoundKey o) {
-            return ComparisonChain.start().compare(count, o.count)
-                    .result();
+        public int compare(WritableComparable a, WritableComparable b) {
+            LongWritable o1 = (LongWritable) a;
+            LongWritable o2 = (LongWritable) b;
+            if (o1.get() < o2.get()) {
+                return 1;
+            } else if (o1.get() > o2.get()) {
+                return -1;
+            } else {
+                return 0;
+            }
         }
-
-        @Override
-        public void write(DataOutput out) throws IOException {
-            out.writeUTF(count.toString());
-            out.writeUTF(key.toString());
-        }
-
-        @Override
-        public void readFields(DataInput in) throws IOException {
-            count = Integer.valueOf(in.readUTF());
-            key = new Text(in.readUTF());
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            CompoundKey that = (CompoundKey) o;
-            return Objects.equals(key, that.key);
-        }
-
-        @Override
-        public int hashCode() {
-            System.out.println("hashcode");
-            return Objects.hash(key);
-        }
-
-        @Override
-        public String toString() {
-            return "CompoundKey{" +
-                    "count=" + count +
-                    ", key=" + key +
-                    '}';
-        }
-    }
-
-    public static Set<String> getSortedSetFromIterable(Iterable<Text> iterable) {
-        Set<String> set = new TreeSet<String>();
-        for (Text value : iterable) {
-            set.add(value.toString());
-        }
-        return set;
     }
 }
